e56b5fc73a84c453384f37e66fdf4cee8a97a4d1
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-11-07 22:39:51 -0500

    Initial Commit

---
+++ iExpenseIntro/iExpenseIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseIntroApp.swift
+//  iExpenseIntro
+//
+//  Created by Jason Cox on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
